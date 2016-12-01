9f164dc280a86b8659c0246d689a7a0a88ef8064
op5 System <root@localhost>
2016-12-01 09:18:27 +0000

    Fixed VMware Snapshot Report event script

--- custom/vmware_snapshot_report.sh
+++ custom/vmware_snapshot_report.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 #Author: Larry Titus
-#Date: March 20th 2015
+#Date: December 1st 2016
 #Reference: https://kb.op5.com/display/HOWTOs/Using+eventhandlers+to+restart+services
 
 state=$1
 statetype=$2
 host=$3
-url=$4
+username=$4
+password=$5
 
 logfile=/opt/monitor/var/eventhandler.log
 
 # Oct 13 2014 14:59:44 GMT
 date=`date +"%b %d %Y %H:%M:%S %Z"`
 case "$1" in
-CRITICAL)
+WARNING)
 if [ "$statetype" = "HARD" ] ; then
-/bin/echo -e "$date vmware_snapshot_report.sh: Got $state and sending a snapshot report for host $host " >> $logfile
+/bin/echo -e "$date vmware_snapshot_report.sh: Got $state and sending report of old snapshots on host $host " >> $logfile
+/opt/plugins/custom/check_snapshot.pl --server $host --username $username --password $password > /tmp/snapshots-$host.txt
+#/opt/plugins/custom/show-all-vmware-snapshots.pl --server $host --username $username --password $password > /tmp/snapshots-$host.txt
+/usr/bin/printf "\nOP5 has detected that host $host has old snapshots. This is a report of snapshots found as of $date."| /usr/bin/mutt -e "my_hdr From: sysop <sysop@greenpeace.org>" -s "[op5] PROBLEM: Old Snapshots found on $host" -i "/tmp/snapshots-$host.txt" global-apps-group@greenpeace.org
+/bin/rm /tmp/snapshots-$host.txt
 
-/opt/plugins/custom/wkhtmltoimage-amd64 $url /tmp/snapshot-$host.jpg > /dev/null 2>&1
-/usr/bin/asmonitor /opt/plugins/custom/check_snapshot.pl --server $host --username x --password x
-
-/bin/echo "OP5 has detected that host $host is in state $state. Attached is a screenshot of $url taken on $date."| /usr/bin/mutt -e "my_hdr From: sysop <sysop@greenpeace.org>" -s "[op5] PROBLEM: Screenshot of $url" -a "/tmp/snapshot-$host.jpg" global-apps-group@greenpeace.org
-
-/bin/rm /tmp/snapshot-$host.jpg
 fi
 ;;
 esac
