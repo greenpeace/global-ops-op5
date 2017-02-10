82eebf2a73f3e0b8fda0fa654904a8192215fd8f
op5 System <root@localhost>
2017-02-10 06:45:50 +0000

    Updating check commands and scripts

--- custom/screenshot_webpage.sh
+++ custom/screenshot_webpage.sh
@@ -17,7 +17,7 @@ CRITICAL)
 if [ "$statetype" = "HARD" ] ; then
 /bin/echo -e "$date screenshot_webpage.sh: Got $state and taking screenshot of $url on host $host " >> $logfile
 /opt/plugins/custom/wkhtmltoimage-amd64 $url /tmp/snapshot-$host.jpg > /dev/null 2>&1
-/bin/echo "OP5 has detected that host $host is in state $state. Attached is a screenshot of $url taken on $date."| /usr/bin/mutt -e "my_hdr From: sysop <sysop@greenpeace.org>" -s "[op5] PROBLEM: Screenshot of $url" -a "/tmp/snapshot-$host.jpg" global-apps-group@greenpeace.org
+/bin/echo "OP5 has detected that host $host is in state $state. Attached is a screenshot of $url taken on $date."| /usr/bin/mutt -e "my_hdr From: sysop <sysop@greenpeace.org>" -s "[op5] PROBLEM: Screenshot of $url" -a "/tmp/snapshot-$host.jpg" -- global-apps-group@greenpeace.org
 /bin/rm /tmp/snapshot-$host.jpg
 fi
 ;;
--- custom/vmware_snapshot_report.sh
+++ custom/vmware_snapshot_report.sh
@@ -14,12 +14,12 @@ logfile=/opt/monitor/var/eventhandler.log
 # Oct 13 2014 14:59:44 GMT
 date=`date +"%b %d %Y %H:%M:%S %Z"`
 case "$1" in
-WARNING)
+WARNING | CRITICAL)
 if [ "$statetype" = "HARD" ] ; then
 /bin/echo -e "$date vmware_snapshot_report.sh: Got $state and sending report of old snapshots on host $host " >> $logfile
 /opt/plugins/custom/check_snapshot.pl --server $host --username $username --password $password > /tmp/snapshots-$host.txt
 #/opt/plugins/custom/show-all-vmware-snapshots.pl --server $host --username $username --password $password > /tmp/snapshots-$host.txt
-/usr/bin/printf "\nOP5 has detected that host $host has old snapshots. This is a report of snapshots found as of $date."| /usr/bin/mutt -e "my_hdr From: sysop <sysop@greenpeace.org>" -s "[op5] PROBLEM: Old Snapshots found on $host" -i "/tmp/snapshots-$host.txt" global-apps-group@greenpeace.org
+/usr/bin/printf "\nOP5 has detected that host $host has old snapshots. This is a report of snapshots found as of $date."| /usr/bin/mutt -e "my_hdr From: sysop <sysop@greenpeace.org>" -s "[op5] PROBLEM: Old Snapshots found on $host" -i "/tmp/snapshots-$host.txt" -- global-apps-group@greenpeace.org
 /bin/rm /tmp/snapshots-$host.txt
 
 fi
